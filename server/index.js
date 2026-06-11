9fe1712dbfe53200897e9ccff31c5cc3866bfbd8
shubhlc194 <shubhamkc194@gmail.com>
2026-06-11 15:36:27 +0530

    setup the project add db middleware

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import console from "node:console";
+import dotenv from "dotenv"
+dotenv.config();
+const app=express();
+ const PORT=process.env.PORT || 6000;
+app.get("/",(req,res)=>{
+    return res.json({message:"server started"})
+});
+app.listen(PORT,()=>{
+    console.log(`server running at port ${PORT}`);
+})
